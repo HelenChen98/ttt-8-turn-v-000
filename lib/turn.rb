dd97f64d6266b3b70e913680ee848a530232e3f1
Karen C <chenyanhuan66@163.com>
2018-07-11 07:54:28 +0000

    automatically backed up by learn

---
+++ lib/turn.rb
@@ -0,0 +1,36 @@
+def display_board(board)
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
+
+def input_to_index(index)
+  return index.to_i-1
+end
+
+def valid_move?(board, index)
+  if board[index]!="X"&&board[index]!="O"&&index.between?(0,8)
+    return true
+  else
+    return false
+  end
+end
+
+def move(board, index, char)
+  board[index]=char
+end
+
+def turn(board)
+  puts "Please enter 1-9:"
+  input = gets
+  if valid_move?(board, input_to_index(input))
+    move(board, input_to_index(input), char)
+  else
+    while !valid_move?(board, input_to_index(input))
+    puts "Please enter 1-9:"
+    input = gets
+  end
+end
+    
